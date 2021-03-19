ed11148af24c001cabfe59c6b868a10d6cd7bca2
Ramiro Andrade <randrade@personalsoft.com.co>
2021-03-19 15:58:07 -0500

    cambios

--- lib/application.ex
+++ lib/application.ex
@@ -1,19 +1,34 @@
-defmodule CusoElixir.Application do
-  use Application
+# defmodule CusoElixir.Application do
+#   use Application
 
-  @impl true
+#   @impl true
+#   def start(_type, _args) do
+#     # Although we don't use the supervisor name below directly,
+#     # it can be useful when debugging or introspecting the system.
+#     children = [
+#       #Cron.Programacion,
+#       #CursoElixir.Repo,
+#       {CursoElixir.Registry, name: CursoElixir.Registry}
+#     ]
+
+#     opts = [strategy: :one_for_one, name: CusoElixir.Supervisor]
+
+#     Supervisor.start_link(children, opts) # {:ok, pid}
+#   end
+
+# end
+defmodule CursoElixir.Application do #Application
+use Application
   def start(_type, _args) do
-    # Although we don't use the supervisor name below directly,
-    # it can be useful when debugging or introspecting the system.
+
+
     children = [
-      #Cron.Programacion,
-      #CursoElixir.Repo,
-      {CursoElixir.Registry, name: CursoElixir.Registry}
+      Cron.Programacion,
+      CursoElixir.Repo,
     ]
 
-    opts = [strategy: :one_for_one, name: CusoElixir.Supervisor]
-
-    Supervisor.start_link(children, opts) # {:ok, pid}
+    opts = [strategy: :one_for_one, name: CursoElixir.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 
 end
